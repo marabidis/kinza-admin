f06cc6ea1393af5343d25eb8985cdc97ed37dd8f
marabidis <i@marabidis.ru>
2023-09-25 18:02:11 +0300

    fix(strapi): strapi init

---
+++ src/api/kinza/controllers/kinza.ts
@@ -0,0 +1,7 @@
+/**
+ * kinza controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::kinza.kinza');
---
+++ src/api/kinza/routes/kinza.ts
@@ -0,0 +1,7 @@
+/**
+ * kinza router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::kinza.kinza');
