10ba85f79f45797d9e8a3651e8037215f9a42b17
marabidis <i@marabidis.ru>
2024-06-07 14:13:33 +0400

    fix(order): fixed n8n url

--- src/api/order/content-types/order/lifecycles.ts
+++ src/api/order/content-types/order/lifecycles.ts
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 export default {
     afterCreate({ result }) {
-      axios.post('http://localhost:5678/webhook-test/5896e17a-8bc7-4f72-9e4c-81a982d855e2', result).catch(() => {})
+      axios.post('http://n8n:5678/webhook/5896e17a-8bc7-4f72-9e4c-81a982d855e2', result).catch((error) => {
+
+        console.error(error);
+      })
     },
   };
