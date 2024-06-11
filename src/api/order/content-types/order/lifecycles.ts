efbfb3d589dc27512bd8856ad2bc313b12540e6c
marabidis <i@marabidis.ru>
2024-06-11 12:50:11 +0400

    feat(n8n email): recreated n8n local

--- src/api/order/content-types/order/lifecycles.ts
+++ src/api/order/content-types/order/lifecycles.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
     afterCreate({ result }) {
-      axios.post('http://n8n:5678/webhook/5896e17a-8bc7-4f72-9e4c-81a982d855e2', result).catch((error) => {
+      axios.post(`http://n8n:5678/webhook/${process.env.N8N_WEBHOOK_ID}`, result).catch((error) => {
 
         console.error(error);
       })
