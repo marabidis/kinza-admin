169bef13a8935e62092dd3c94fd8c1d9508b58cb
marabidis <i@marabidis.ru>
2025-05-23 22:30:49 +0400

    fix(upload): fixed config for s3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,24 +1,50 @@
 export default ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
-            s3Options: {
-
-              credentials: {
-                accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                secretAccessKey: env('AWS_ACCESS_SECRET'),
-              },
-            //     accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            //     secretAccessKey: env('AWS_ACCESS_SECRET'),
-                region: 'ru-central1',
-            //     endpoint:'https://storage.yandexcloud.net',
-                params: {
-                  Bucket: env('AWS_BUCKET'),
-                  CacheControl: "public, max-age=864000"
-                },
-            }
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: 'ru-central1',
+        endpoint: env('AWS_ENDPOINT', 'https://storage.yandexcloud.net'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+          CacheControl: "public, max-age=864000",
         },
+        s3ForcePathStyle: true,
       },
     },
+  },
 });
+
+// export default ({ env }) => ({
+//     upload: {
+//       config: {
+//         provider: 'aws-s3',
+//         providerOptions: {
+//             // s3Options: {
+
+//               credentials: {
+//                 accessKeyId: env('AWS_ACCESS_KEY_ID'),
+//                 secretAccessKey: env('AWS_ACCESS_SECRET'),
+//               },
+//             //     accessKeyId: env('AWS_ACCESS_KEY_ID'),
+//             //     secretAccessKey: env('AWS_ACCESS_SECRET'),
+//                 region: 'ru-central1',
+//             //     endpoint:'https://storage.yandexcloud.net',
+//                 params: {
+//                   Bucket: env('AWS_BUCKET'),
+//                   CacheControl: "public, max-age=864000"
+//                 },
+
+//                  endpoint: env('AWS_ENDPOINT', 'https://storage.yandexcloud.net'),
+//                  s3Options: {
+//                       s3ForcePathStyle: true,
+//                  },
+//         // или для некоторых версий Strapi:
+//                  forcePathStyle: true,
+//                  }
+//              },
+//       // },
+//     },
+// });
